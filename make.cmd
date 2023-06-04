f95a0dc7681a1a4cc789b78e95913a1ae8e54cb6
prokushev <yuri.prokushev@gmail.com>
2023-06-04 22:09:45 +0700

    CP/M now in separate repo

--- make.cmd
+++ make.cmd
@@ -1,35 +1,8 @@
-asw -lU basic80.asm > basic80.lst
-p2bin basic80.p basic80.bin
-exit
-
-asw -lU mon580-306-vt52.asm > mon580-306-vt52.lst
-p2bin mon580-306-vt52.p mon580-306-vt52.bin
-
-exit
 
 asw -lU monitor.asm > monitor.lst
 p2bin monitor.p monitor.bin
 
-asw -lU cpm64-term.asm > cpm64-term.lst
-p2bin cpm64-term.p cpm64-term.bin
-
-asw -lU cpm64-bios.asm > cpm64-bios.lst
-p2bin cpm64-bios.p cpm64-bios.bin
-
-asw -lU cpm64-bdos.asm > cpm64-bdos.lst
-p2bin cpm64-bdos.p cpm64-bdos.bin
-
-asw -lU cpm64-ccp.asm > cpm64-ccp.lst
-p2bin cpm64-ccp.p cpm64-ccp.bin
-
-asw -lU cpm64-loader.asm > cpm64-loader.lst
-p2bin cpm64-loader.p cpm64-loader.bin
-
 asw -lU ch-com.asm > ch-com.lst
 p2bin ch-com.p ch-com.bin
 
-bin2rk cpm64-loader.bin 12544
 
-bin2rk basic80.bin
-
-bin2rk ctrlbas.bin
