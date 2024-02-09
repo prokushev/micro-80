f31e9fbda35cc2f90918c2f9617082eceb8fae75
prokushev <yuri.prokushev@gmail.com>
2024-02-09 16:17:10 +0700

    romctrl as submodule

--- make.cmd
+++ make.cmd
@@ -1,8 +1,15 @@
+@echo off
+
 asw -lU monitor.asm > monitor.lst
 p2bin monitor.p monitor.bin
 
 asw -lU monitor-rk86.asm > monitor-rk86.lst
 p2bin monitor-rk86.p monitor-rk86.bin
 
-asw -lU romdisk.asm > romdisk.lst
+echo ROMCTRL
+cd src\romctrl
+call make
+cd ..\..
+
+asw -lU -i src\romctrl\roms romdisk.asm > romdisk.lst
 p2bin romdisk.p romdisk.bin
