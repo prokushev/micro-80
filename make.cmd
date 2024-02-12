a124e1d671fddd492b54697306a239579f883d49
prokushev <yuri.prokushev@gmail.com>
2024-02-12 20:23:31 +0700

    cleanup, sort

--- make.cmd
+++ make.cmd
@@ -4,11 +4,11 @@ SET PATH=tools;%PATH%
 
 echo MONITOR
 asw -q -lU src\monitor\monitor.asm > monitor.lst
-p2bin src\monitor\monitor.p monitor.bin
+p2bin src\monitor\monitor.p rom\monitor.bin
 
 echo MONITOR RADIO-86RK
 asw -lU src\m80k\monitor-rk86.asm > monitor-rk86.lst
-p2bin src\m80k\monitor-rk86.p monitor-rk86.bin
+p2bin src\m80k\monitor-rk86.p rom\monitor-rk86.bin
 
 echo ROMCTRL
 cd src\romctrl
@@ -16,5 +16,5 @@ call make
 cd ..\..
 
 echo ROM-disk
-asw -lU -i src\romctrl\roms romdisk.asm > romdisk.lst
-p2bin romdisk.p romdisk.bin
+asw -lU -i src\romctrl\roms -i bin src\romdisk\romdisk.asm > romdisk.lst
+p2bin src\romdisk\romdisk.p rom\romdisk.bin
